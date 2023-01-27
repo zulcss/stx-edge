9194e3fa2347d72436ab06b32be5a632f1799682
Chuck SHort <zulcss@gmail.com>
2023-01-27 13:16:08 -0500

    rework image pull
    Ask for image to be pulled again if it exists locally.
    
    Signed-off-by: Chuck SHort <zulcss@gmail.com>

--- stx-builder/pkg/images/create.go
+++ stx-builder/pkg/images/create.go
@@ -2,8 +2,10 @@ package images
 
 import (
 	"fmt"
-	"github.com/zulcss/stx-edge/pkg/utils"
 	log "github.com/sirupsen/logrus"
+	"github.com/erikgeiser/promptkit/confirmation"
+
+	"github.com/zulcss/stx-edge/pkg/utils"
 )
 
 type Image struct { 
@@ -19,13 +21,23 @@ func NewImageContext() *Image {
 // Pull an image from a Docker registry
 func (i *Image) CreateImage() error {
 	log.Debug("in CreateImage")
-	i.FetchImage()
+
+	exist := i.ImageExists()
+	if exist {
+		input := confirmation.New("Image already exists, do you want to update it?",
+			confirmation.Undecided)
+		update, _:= input.RunPrompt()
+		if !update {
+			return nil
+		}
+	}
+	i.ImageFetch()
 
 	return nil
 }
 
 // Pull a docker image from a registry
-func (i *Image) FetchImage() error {
+func (i *Image) ImageFetch() error {
 	log.Debug("in FetchImage")
 
 	out, err := utils.SH(fmt.Sprintf("docker image pull %s", i.ImageTag))
@@ -33,5 +45,16 @@ func (i *Image) FetchImage() error {
 		log.Errorf("Failed to pull image: %s", out)
 		return err
 	}
+	log.Infof("Succesfully pulled %s", i.ImageTag)
 	return nil
+}
+
+func (i *Image) ImageExists() bool{
+	log.Debug("in ImageExists")
+	_, err := utils.SH(fmt.Sprintf("docker inspect --type=image %s", i.ImageTag))
+	if err == nil {
+	   log.Infof("Image is present locally: %s", i.ImageTag)
+	   return true
+	}
+	return false
 }
